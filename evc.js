7d68c6688b2a0c471cc32c74fc5b0995e6642c51
Hassan Aw <awale822@gmail.com>
2024-02-10 21:57:52 +0300

    evc program front-end updated

--- evc.js
+++ evc.js
@@ -81,3 +81,23 @@ function billPayment() {
      outputDiv1.textContent = "Enter your Bill number:";
 }
 
+// let one = document.getElementById("one");
+// let two = document.getElementById("two");
+// let three = document.getElementById("three");
+// let four = document.getElementById("four");
+// let five = document.getElementById("five");
+// let six = document.getElementById("six");
+// let seven = document.getElementById("seven");
+// let eight = document.getElementById("eight");
+
+// let nine = document.getElementById("nine");
+// let zero = document.getElementById("zero");
+// let star = document.getElementById("star");
+// let hash = document.getElementById("hash");
+// let display = document.getElementById("display");
+
+// one.onclick = function(){
+//   document.getElementById("display").innerText = one;
+// }
+
+
